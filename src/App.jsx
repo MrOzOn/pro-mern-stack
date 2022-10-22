708639486029601bf8be0037eb0793b7fb0f0e20
MrOzOn <mr.ozon1982@gmail.com>
2022-10-22 12:02:17 +0300

    add dynamic rows

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
 const contentNode = document.getElementById('contents');
 
+const issues = [
+  {
+    id: 1, status: 'Open', owner: 'Ravan',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add',
+  }, {
+    id: 2, status: 'Assigned', owner: 'Eddie', 
+    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel',
+  }, 
+];
+
+class IssueRow extends React.Component {
+    render() {
+      const issue = this.props.issue;
+      // const borderedStyle = {border: "1px solid silver", padding: 4};
+      return (
+        <tr>
+          <td>{issue.id}</td>
+          <td>{issue.status}</td>
+          <td>{issue.owner}</td> 
+          <td>{issue.created.toDateString()}</td> 
+          <td>{issue.effort}</td> 
+          <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td> 
+          <td>{issue.title}</td>
+        </tr> 
+        )
+} }
+
 class IssueFilter extends React.Component {
     render() {
         return (
@@ -9,14 +38,31 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render() {
+        const borderedStyle = {border: "1px solid silver", padding: 6};
+        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
         return (
-        <div>This is a placeholder for a table of Issues.</div>
-    ) }
+          <table className="bordered-table">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Status</th>
+                <th>Owner</th>
+                <th>Created</th>
+                <th>Effort</th>
+                <th>Completion Date</th>
+                <th>Title</th>
+              </tr>
+            </thead>
+            <tbody>
+              {issueRows}
+            </tbody>
+      </table> )
+  }
 }
 
 class IssueAdd extends React.Component {
         render() {
-        return (
+            return (
             <div>This is a placeholder for an Issue Add entry form.</div>
     ) }
 }
@@ -28,7 +74,7 @@ class IssueList extends React.Component {
             <h1>Issue Tracker</h1>
             <IssueFilter />
             <hr />
-            <IssueTable />
+            <IssueTable issues={issues}/>
             <hr />
             <IssueAdd />
         </div>
